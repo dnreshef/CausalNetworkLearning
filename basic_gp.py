8783465734edc9b423bd5eab390f38d27776910a
George Du <gdu@mit.edu>
2015-04-06 05:36:49 -0400

    update

--- basic_gp.py
+++ basic_gp.py
@@ -31,11 +31,12 @@ def normalize_data(X):
 
 # Computes the q-th percentile for the pairwise euclidean distances in X.
 def edistance_at_percentile(X, q):
+    return 1
     distances = []
     for i in xrange(10000):
         a = np.random.choice(X)
         b = np.random.choice(X)
-        distances.append((a - b)**2)
+        distances.append((a - b) ** 2)
     distances.sort()
     for i in xrange(q*10, 10000):
         if distances[i] > 0:
@@ -108,7 +109,7 @@ def gp_bandwidths(X, Y, filename):
     return [min(score_type) for score_type in zip(*scores)]
 
 # Process all data from cause-effect database
-with open(prefix + "scores_test.csv", "w") as f:
+with open(prefix + "scores_test.csv", "a") as f:
     filenum = 1
     filenum2 = 89
     if len(sys.argv) >= 2:
@@ -117,7 +118,7 @@ with open(prefix + "scores_test.csv", "w") as f:
             filenum2 = int(sys.argv[2])
         else:
             filenum2 = filenum + 1
-    f.write("fileno,var_xy,var_yx,MI_xy,MI_yx,NLL_xy,NLL_yx,MIC_xy,MIC_yx,var_xy_norm," +
+    f.write("fileno,var_xy,var_yx,MI_xy,MI_yx,NLL_xy,NLL_yx,MIC_xy,MIC_yx,var_xy_norm" +
         "var_yx_norm,MI_xy_norm,MI_yx_norm,NLL_xy_norm,NLL_yx_norm,MIC_xy_norm,MIC_yx_norm\n")
     for i in xrange(filenum, filenum2):
         if i == 47:
@@ -125,7 +126,7 @@ with open(prefix + "scores_test.csv", "w") as f:
             continue
         filename = "pair00%02d.txt" % (i,)
         X, Y = loaddata(filename)
-        if X.size > 2000:
+        if X.size > 5000:
             print "Size of file %d too big (%d)\n" % (i, X.size)
             continue
         if Y.shape[1] > 1:
@@ -135,7 +136,7 @@ with open(prefix + "scores_test.csv", "w") as f:
         X_norm, Y_norm = normalize_data(X), normalize_data(Y)
         scores = gp_bandwidths(X, Y, prefix + "%02d" % (i,))
         scores_norm = gp_bandwidths(X_norm, Y_norm, prefix + "%02dnorm" % (i,))
-        print "x --> y (MI): %s %s" % (scores[2], scores_norm[2])
-        print "x <-- y (MI): %s %s\n" % (scores[3], scores_norm[3])
+        #print "x --> y (MI): %s" % (scores[2],)
+        #print "x <-- y (MI): %s\n" % (scores[3],)
         output = [i] + scores + scores_norm
         f.write("%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % tuple(output))
