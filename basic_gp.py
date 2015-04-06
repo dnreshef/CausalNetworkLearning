87a7714bdc832f8bdd82743724083c6cd715f698
George Du <gdu@mit.edu>
2015-04-06 12:10:12 -0400

    each line in results commits persistently even if program terminates

--- basic_gp.py
+++ basic_gp.py
@@ -110,33 +110,34 @@ def gp_bandwidths(X, Y, filename):
 
 # Process all data from cause-effect database
 with open(prefix + "scores_test.csv", "a") as f:
-    filenum = 1
-    filenum2 = 89
-    if len(sys.argv) >= 2:
-        filenum = int(sys.argv[1])
-        if len(sys.argv) > 2:
-            filenum2 = int(sys.argv[2])
-        else:
-            filenum2 = filenum + 1
     f.write("fileno,var_xy,var_yx,MI_xy,MI_yx,NLL_xy,NLL_yx,MIC_xy,MIC_yx,var_xy_norm" +
         "var_yx_norm,MI_xy_norm,MI_yx_norm,NLL_xy_norm,NLL_yx_norm,MIC_xy_norm,MIC_yx_norm\n")
-    for i in xrange(filenum, filenum2):
-        if i == 47:
-            print "pair0047.txt is too hard\n"
-            continue
-        filename = "pair00%02d.txt" % (i,)
-        X, Y = loaddata(filename)
-        if X.size > 5000:
-            print "Size of file %d too big (%d)\n" % (i, X.size)
-            continue
-        if Y.shape[1] > 1:
-            print "File %d has multidimensional data\n" % (i,)
-            continue
-        print "Running GP on %s, %d data points" % (filename, Y.size)
-        X_norm, Y_norm = normalize_data(X), normalize_data(Y)
-        scores = gp_bandwidths(X, Y, prefix + "%02d" % (i,))
-        scores_norm = gp_bandwidths(X_norm, Y_norm, prefix + "%02dnorm" % (i,))
-        #print "x --> y (MI): %s" % (scores[2],)
-        #print "x <-- y (MI): %s\n" % (scores[3],)
-        output = [i] + scores + scores_norm
+filenum = 1
+filenum2 = 89
+if len(sys.argv) >= 2:
+    filenum = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        filenum2 = int(sys.argv[2])
+    else:
+        filenum2 = filenum + 1
+for i in xrange(filenum, filenum2):
+    if i == 47:
+        print "pair0047.txt is too hard\n"
+        continue
+    filename = "pair00%02d.txt" % (i,)
+    X, Y = loaddata(filename)
+    if X.size > 5000:
+        print "Size of file %d too big (%d)\n" % (i, X.size)
+        continue
+    if Y.shape[1] > 1:
+        print "File %d has multidimensional data\n" % (i,)
+        continue
+    print "Running GP on %s, %d data points" % (filename, Y.size)
+    X_norm, Y_norm = normalize_data(X), normalize_data(Y)
+    scores = gp_bandwidths(X, Y, prefix + "%02d" % (i,))
+    scores_norm = gp_bandwidths(X_norm, Y_norm, prefix + "%02dnorm" % (i,))
+    #print "x --> y (MI): %s" % (scores[2],)
+    #print "x <-- y (MI): %s\n" % (scores[3],)
+    output = [i] + scores + scores_norm
+    with open(prefix + "scores_test.csv", "a") as f:
         f.write("%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % tuple(output))
