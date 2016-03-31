9df500923306f24188be81bfbfe1c69e466371b3
George Du <gdu@mit.edu>
2016-03-31 18:10:24 -0400

    edit run_all to survive ssh termination

--- spring16/run_all.sh
+++ spring16/run_all.sh
@@ -1,4 +1,4 @@
 for simulation in line plane parabola paraboloid sine corrugated_curve
 do
-    ./gp_gpy.py 150 $simulation > /dev/null &
+    nohup ./gp_gpy.py 150 $simulation > /dev/null 2>&1 &
 done
